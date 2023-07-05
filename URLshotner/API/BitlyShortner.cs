2403af396075a4787f4220d6f036a46bf4a2d15e
LuisEduardoSpindola <eduardospindola.alves@gmail.com>
2023-07-05 16:39:34 -0300

    Adicionar arquivos de projeto.

---
+++ URLshotner/API/BitlyShortner.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+public class BitlyShortener
+{
+    private string apiKey;
+
+    public BitlyShortener(string apiKey)
+    {
+        this.apiKey = apiKey;
+    }
+
+    public string ShortenUrl(string longUrl)
+    {
+        using (var client = new WebClient())
+        {
+            client.Headers.Add("Authorization", $"Bearer {apiKey}");
+            client.Headers.Add("Content-Type", "application/json");
+
+            var requestData = new { long_url = longUrl };
+            
+            string requestBody = JsonConvert.SerializeObject(requestData);
+
+            try
+            {
+                string response = client.UploadString("https://api-ssl.bitly.com/v4/shorten", "POST", requestBody);
+                dynamic responseData = JsonConvert.DeserializeObject(response);
+                string shortUrl = responseData.link;
+
+                return shortUrl;
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine("Erro na solicitação: " + ex.Message);
+            }
+        }
+
+        return null;
+    }
+}
---
+++ URLshotner/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string apiKey = "3dcd51995df8dcc2ca58c5565ddfca919f937e39";
+
+        BitlyShortener shortener = new BitlyShortener(apiKey);
+
+        Console.WriteLine("--- Encurtador de URL ---");
+        Console.WriteLine("");
+        Console.Write("URL:");
+        string longUrl = Console.ReadLine();
+
+        string shortUrl = shortener.ShortenUrl(longUrl);
+
+        Console.WriteLine("URL encurtada: " + shortUrl);
+    }
+}
